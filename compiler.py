e9f3d57d18e6e8c3b15ad2c4ec19a6a8bd23fddf
Luis Garcia <luisgarmir@hotmail.com>
2021-11-24 17:02:05 -0600

    changed from literals to regex expressions, more tokens added

--- compiler.py
+++ compiler.py
@@ -1,16 +1,27 @@
 import ply.yacc as yacc
 import ply.lex as lex
 
-literals = ['=', '+', '-', '*', '/', '(', ')']
+# literals = ['=', '+', '-', '*', '/','^', '(', ')','==','!=', '>=','<=','>','<','{','}']
+
 reserved = { 
     'int' : 'INTDEC',
     'float' : 'FLOATDEC',
-    'print' : 'PRINT'
+    'print' : 'PRINT',
  }
 
 tokens = [
-    'INUMBER', 'FNUMBER', 'NAME'
+    'INUMBER', 'FNUMBER', 'NAME', 'PLUS', 'TIMES', 'LPAREN', 'RPAREN', 'MINUS', 'DIVIDE','EQUALS', 'ASSIGN'
 ] + list(reserved.values())
+
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_EQUALS  = r'=='
+t_ASSIGN = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+# t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 # Tokens
 def t_NAME(t):
@@ -81,7 +92,7 @@ def p_statement_declare_float(p):
     names[p[2]] = { "type": "FLOAT", "value":p[3]}
 
 def p_is_assing(p):
-    '''is_assing : "=" expression 
+    '''is_assing : ASSIGN expression 
                 | '''
     p[0] = 0
     # p[0] = Node(0,'INT',[])
@@ -96,7 +107,7 @@ def p_statement_print(p):
     print(p[3])
 
 def p_statement_assign(p):
-    'statement : NAME "=" expression'
+    'statement : NAME ASSIGN expression'
     if p[1] not in names:
         print ( "You must declare a variable before using it")
     names[p[1]]["value"] = p[3]
