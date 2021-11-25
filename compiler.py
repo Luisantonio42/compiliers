f8dc80e5fae6cb7ed28546ce368974c80677a8c0
Luis Garcia <luisgarmir@hotmail.com>
2021-11-25 13:24:09 -0600

    Changed expression declaration because i wasnt able to finish sentences with ;

--- compiler.py
+++ compiler.py
@@ -1,39 +1,59 @@
 import ply.yacc as yacc
 import ply.lex as lex
 
-reserved = { 
+reserved = {
+    # datatypes 
     'int' : 'INTDEC',
     'float' : 'FLOATDEC',
-    'print' : 'PRINT',
     'string': 'STRINGDEC',
     'boolean': 'BOOLEANDEC',
+
+    # booleans
     'true': 'TRUE',
     'false':'FALSE',
     'and': 'AND',
-    'or': 'OR'
+    'or': 'OR',
+
+    # cotrol flow
+    'if': 'IF',
+    'elif': 'ELIF',
+    'else': 'ELSE',
+
+    # print
+    'print' : 'PRINT'
  }
 
 tokens = [
     'INUMBER', 'FNUMBER', 'NAME', 'PLUS', 'TIMES','EXP',
     'LPAREN', 'RPAREN', 'MINUS', 'DIVIDE','EQUALS', 'ASSIGN', 
     'STRING', 'NOT_EQUALS', 'M_EQUALS', 'L_EQUALS', 'MORE', 
-    'LESS'
+    'LESS', 'LKEY', 'RKEY', 'FINISH'
 ] + list(reserved.values())
 
+# arithmetic ops 
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
 t_EXP = r'\^'
+
+# comparassion
 t_EQUALS  = r'=='
 t_NOT_EQUALS  = r'!='
 t_M_EQUALS = r'>='
 t_L_EQUALS = r'<='
 t_LESS = r'<'
 t_MORE = r'>'
+
+# assigns
 t_ASSIGN = r'='
+
+# block ops
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
+t_LKEY = r'{'
+t_RKEY = r'}'
+t_FINISH = r';'
 
 # Tokens
 def t_NAME(t):
@@ -85,39 +105,18 @@ precedence = (
 names = {}
 abstractTree = []
 
-def p_statement_declare_int(p):
-    '''statement : INTDEC NAME is_assing
-    '''
-    if type(p[3]) == float:
-        print("You can not assing a float to an integer")
-    else:
-        names[p[2]] = { "type": "INT", "value": p[3]}
-
-def p_statement_declare_float(p):
+# declarations 
+def p_statement_declare_var(p):
     'statement : FLOATDEC NAME is_assing'
     names[p[2]] = { "type": "FLOAT", "value":p[3]}
 
-def p_statement_declare_string(p):
-    'statement : STRINGDEC NAME is_assing'
-    if type(p[3]) == float or type(p[3]) == int:
-        print("You need to use quotes("") to declare a number as a string ")
-    else:
-        names[p[2]] = { "type": "STRING", "value":p[3]}
-
-def p_statement_declare_boolean(p):
-    'statement : BOOLEANDEC NAME is_assing'
-    names[p[2]] = { "type": "BOOLEAN", "value":p[3]}
-
+# assigns
 def p_is_assing(p):
     '''is_assing : ASSIGN expression 
                 | '''
     p[0] = 0
     if len(p) > 2:
         p[0] = p[2]
-
-def p_statement_print(p):
-    '''statement : PRINT LPAREN expression RPAREN '''
-    print(p[3])
 
 def p_statement_assign(p):
     'statement : NAME ASSIGN expression'
@@ -126,12 +125,24 @@ def p_statement_assign(p):
     else:
         names[p[1]]["value"] = p[3]
 
-
 def p_statement_expr(p):
     'statement : expression'
-    print(p[1])
+    print("hola",p[1])
 
 
+# print
+def p_statement_print(p):
+    '''statement : PRINT LPAREN expression RPAREN '''
+    print(p[3])
+
+# Control flow
+# def p_statement_if_else(p):
+#     '''statement_if_else : if_cond'''
+#     p[0] = ('condition', p[1], p[2], p[3])
+
+def p_statement(p):
+    '''statement : statement FINISH statement'''
+# Operations
 def p_expression_binop(p):
     '''expression : expression PLUS expression
                   | expression MINUS expression
@@ -146,34 +157,9 @@ def p_expression_binop(p):
                   | expression LESS expression
                   | expression AND expression
                   | expression OR expression'''
-    # p[0] = ('operation',p[1],p[2],p[3])
-    if p[2] == '+':
-        p[0] = p[1] + p[3]
-    elif p[2] == '-':
-        p[0] = p[1] - p[3]
-    elif p[2] == '*':
-        p[0] = p[1] * p[3]
-    elif p[2] == '/':
-        p[0] = p[1] / p[3]
-    elif p[2] == '==':
-        p[0] = p[1] == p[3]
-    elif p[2] == '!=':
-        p[0] = p[1] != p[3]
-    elif p[2] == '>':
-        p[0] = p[1] > p[3]
-    elif p[2] == '<':
-        p[0] = p[1] < p[3]
-    elif p[2] == '>=':
-        p[0] = p[1] >= p[3]
-    elif p[2] == '<=':
-        p[0] = p[1] <= p[3]
-    elif p[2] == '^':
-        p[0] = p[1] ** p[3]
-    elif p[2] == 'and':
-        p[0] = p[1] and p[3]
-    elif p[2] == 'or':
-        p[0] = p[1] or p[3]
+    p[0] = ('operation',p[1],p[2],p[3])
 
+# expressions
 def p_expression_uminus(p):
     "expression : '-' expression %prec UMINUS"
     p[0] = -p[2]
@@ -183,23 +169,22 @@ def p_expression_group(p):
     "expression : '(' expression ')'"
     p[0] = p[2]
 
-
-def p_expression_inumber(p):
-    "expression : INUMBER"
+def p_types(p):
+    '''type : INT
+            | FLOAT
+            | STRING
+            | BOOLEAN'''
     p[0] = p[1]
 
-
-def p_expression_fnumber(p):
-    "expression : FNUMBER"
+def p_expression_dec(p):
+    """expression : INUMBER
+                | FNUMBER
+                | STRING
+                | boolean_dec"""
     p[0] = p[1]
 
-def p_expression_string(p):
-    "expression : STRING"
-    p[0] = p[1]
-    # print(p[0])
-
-def p_expression_bool(p):
-    '''expression : TRUE
+def p_expression_boolean(p):
+    '''boolean_dec : TRUE
               | FALSE'''
     if p[1] == "true":
         p[0] = True
@@ -214,7 +199,24 @@ def p_expression_name(p):
         print("Undefined name '%s'" % p[1])
         p[0] = 0
 
+# control flow
+# def p_expression_if(p):
+#     """if_cond : IF LPAREN expression RPAREN LKEY statement RKEY"""
+#     p[0] = ('if', p[3],p[6])
 
+# def p_expression_elif(p):
+#     """elif_cond : ELIF LPAREN expression RPAREN LKEY statement RKEY elif_cond"""
+#     if len(p) > 2:
+#         p[0] = (('elif',p[3], p[6]), ) + p[8]
+#     else:
+#         p[0] = ()
+
+# def p_expression_else(p):
+#     """else_cond : ELSE LKEY statement RKEY"""
+#     if len(p) > 2:
+#         p[0] = ('else',p[3])
+
+# Errors
 def p_error(p):
     if p:
         print(p)
